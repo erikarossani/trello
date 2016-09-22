2adbe77d03deaaa676e786d3bb5bf3fa7975974d
yeiny gonzalez cifuentes <yeinygonzalezcifuentes@gmail.com>
2016-09-22 09:03:36 -0500

    version 2

--- js/app.js
+++ js/app.js
@@ -1,20 +1,51 @@
 window.addEventListener("load", function() {
 	var boton = document.getElementById("trello1");
   var textArea = document.getElementById("texto");
-  var guardar = 
+  var guardar = document.getElementById("resultado");
+  var contenedor = document.getElementById("tema");
+  var retornar = document.getElementById("retornar");
 
 	boton.addEventListener("click", function(e) {
-    e.preventDefault();
-    var guardar = document.getElementById("resultado");
+      e.preventDefault();
 
-    activarCampo();
+      activarCampo();
+      moverCampo()
 
-});
+  });
 
-    function activarCampo(){
-        var contenedor = document.getElementById("tema");
-        contenedor.style.display = "block";
-        return true;
+      function activarCampo(){
+          contenedor.style.display = "block";
+          return true;
     }
 
+  retornar.addEventListener("click", function(e) {
+      e.preventDefault();
+
+      retornarCampo()
+
+  });
+
+      function retornarCampo(){
+          contenedor.style.display = "none";
+          return true;
+    }
+
+  guardar.addEventListener("click", function() {
+     var texto = textArea.value;
+     agregarMensaje(texto);
+
+  });
+
+    function agregarMensaje(texto){
+       var nuevoItem = document.createElement("div");
+       var btn = document.createElement("button");
+       nuevoItem.innerHTML =texto;
+       btn.textContent = "Añadir una tarjeta";
+       btn.classList.add("texto");
+
+       var lista = document.getElementById("tarjeta");
+       lista.insertBefore(btn, tarjeta.childNodes[0]);
+       lista.insertBefore(nuevoItem, tarjeta.childNodes[0]);
+       document.getElementById("texto").value = "";
+    }
 });
