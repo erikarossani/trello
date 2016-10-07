01360298cd44353701223495307f531f2e662e17
yeiny gonzalez cifuentes <yeinygonzalezcifuentes@gmail.com>
2016-10-07 15:03:33 -0500

    animated

--- js/app.js
+++ js/app.js
@@ -1,35 +1,4 @@
 window.addEventListener("load", function() {
-<<<<<<< HEAD
-      var boton = document.getElementById("trello1");
-      var textArea = document.getElementById("texto");
-      var guardar = document.getElementById("resultado");
-      var contenedor = document.getElementById("tema");
-      var retornar = document.getElementById("retornar");
-
-boton.addEventListener("click", function(e) {
-	 e.preventDefault();
-      activarCampo();
-      boton.style.display ="none";
-      textArea.focus();
-});
-
-retornar.addEventListener("click", function(e) {
-	 e.preventDefault();
-	 retornarCampo()
-	 boton.style.display ="block";
-
-	 if (contenedor.style.display = "none") {
-		boton.style.marginLeft = "10";
-	 }
-});
-
-guardar.addEventListener("click", function() {
-	 agregarMensaje(texto, this);
-	 contenedor.style.display = "none";
-	 boton.style.display ="block";
-     moverTarjeta();
-});
-=======
 	var subcontenedor = document.getElementById("subcontenedor");
 	var lista = document.getElementById("lista");
 	var anadirLista = document.getElementById("anadirLista");
@@ -51,11 +20,11 @@ guardar.addEventListener("click", function() {
 		agregarMensaje(inputLista, this);
 		insertarContenedor();
 		inputLista.value = "";
+
 		lista.addEventListener("drop", soltar);
 		lista.addEventListener("dragover", arrastrarSobre);
 		lista.addEventListener("dragleave", dejaArrastrar);
 	});
->>>>>>> c2e4b99018032cceefdb3765973599ccd514d381
 
 	function activarCampo(){
 	     formulario.style.display = "block";
@@ -81,22 +50,6 @@ guardar.addEventListener("click", function() {
 		});
 	}
 
-<<<<<<< HEAD
-=======
-	function retornarCampo(){
-	   formulario.style.display = "none";
-	}
-
-<<<<<<< HEAD
-	function agregarMensaje(texto, boton){
-	     var nuevoItem = document.createElement("div");
-	     var btn = document.createElement("button");
-	     nuevoItem.innerHTML =texto.value;
-	     btn.textContent = "Añadir una tarjeta";
-	     btn.classList.add("texto");
-	     nuevoItem.classList.add("div")
-=======
->>>>>>> bcc967f731bb79559703331c285b4fbbbff52b1d
 	function insertarContenedor(){
 		var nuevoCampo = document.createElement("div");
 		subcontenedor.appendChild(nuevoCampo);
@@ -123,7 +76,6 @@ guardar.addEventListener("click", function() {
 		card.insertBefore(textArea, card.childNodes[0]);
 		card.insertBefore(btnAnadir, card.childNodes[1]);
 		padre.appendChild(card);
->>>>>>> c2e4b99018032cceefdb3765973599ccd514d381
 
 		btnAnadir.type = "button";
 		btnAnadir.innerText = "Añadir";
@@ -140,29 +92,10 @@ guardar.addEventListener("click", function() {
 		});
 	}
 
-<<<<<<< HEAD
-	function moverTarjeta(){
-	      var padre =guardar.parentElement.parentElement;
-	      var mover = document.createElement("div");
-	      principal.appendChild(mover);
-	      mover.appendChild(boton);
-	      mover.appendChild(contenedor);
-	      mover.classList.add("mover");
-	}
-
-	function mostrarCampo(){
-            
-        var item = document.createElement("textarea");
-        var btn1 = document.createElement("button");
-        btn1.textContent = "Añadir";
-        btn1.classList.add("añadir");
-        item.classList.add("item");
-=======
 	function guardarTarjeta(padre,textArea){
 		var campoTarjeta = document.createElement("div");
 		campoTarjeta.innerText = textArea.value;
 		padre.insertBefore(campoTarjeta, padre.lastChild);
->>>>>>> c2e4b99018032cceefdb3765973599ccd514d381
 
 		campoTarjeta.classList.add("tarjeta1");
 		campoTarjeta.setAttribute("id", "card.1"+contador);
@@ -193,14 +126,17 @@ guardar.addEventListener("click", function() {
 	    var temporal = this.innerHTML;
 	    this.insertBefore(elementoArrastrado, this.childNodes[1])
 	    this.style.backgroundColor = "#E2E4E6";
+	    this.classList.add("rotateIn", "animated");
     }
 
     function terminaArrastrar(e) {
 	    this.style.opacity = null;
+
     }
 
     function dejaArrastrar(e) {
 	   this.style.backgroundColor = "#E2E4E6";
+	   this.classList.remove("rotateIn", "animated");
     }
 
 });
